fbd57634843e82f33f8cab8acad8affa2f1d7cbb
Arijit Ray <arijit@flick2know.com>
2024-03-22 02:54:17 +0530

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+## Azure Queue Wrapper for Node.js
+This package provides a convenient wrapper for working with Azure Storage Queues in Node.js applications.
+
+### Installation
+You can install the package using npm:
+
+```bash
+npm install azure-queue-wrapper
+```
+### Usage
+1. Configure the Package
+   First, create a configuration file named aqw.config.json in your project directory with the following content:
+```json
+{
+"azureStorageConnectionString": "<your-azure-storage-connection-string>",
+"retries": 3
+} 
+```
+Replace <your-azure-storage-connection-string> with your actual Azure Storage connection string.
+
+2. Initialize Azure Queue Wrapper
+   Import the package and initialize the Azure Queue Wrapper in your code:
+
+```javascript
+const { ProcessAzureQueueMessage, AddMessageToQueue } = require('azure-queue-wrapper');
+```
+3. Process Messages from Queue
+   Use the @ProcessAzureQueueMessage decorator to process messages from a queue based on a specified time interval:
+```javascript
+class MyQueueProcessor {
+@ProcessAzureQueueMessage({ queue: 'my-queue', timeInterval: [5, 'seconds'], maxRetry: 3, deadLetterQueue: 'poison-queue-name' }) // Replace with your queue name, retries count(default: 3), interval (default:5 seconds) and deadLetterQueue name( default: <queue-name>-poison)
+async processQueueMessage(message) { // Replace with your function name
+// Your message processing logic here
+}
+}
+```
+### Supported Time Units
+
+The following time units are supported for specifying the time interval:
+
+- Seconds
+- Minutes
+- Hours
+- Days
+- Weeks
+
+The time interval should be specified as a tuple with a numeric value and the unit, for example, `(5, 'seconds')` for 5 seconds, `(10, 'minutes')` for 10 minutes, and so on.
+4. Add Message to Queue
+   Use the @AddMessageToQueue decorator to add a message to a queue:
+
+```javascript
+class MyQueueProducer {
+@AddMessageToQueue()
+async addMessageToQueue(queueName, message) { // Your function name
+// Your message adding logic here
+return { status: 'success' };
+}
+}
+```
+Contributing
+Contributions are welcome! Please feel free to submit issues or pull requests on the GitHub repository.
+
+## License
+This project is licensed under the MIT License.
+
---
+++ index.js
@@ -0,0 +1,117 @@
+import {QueueServiceClient} from "@azure/storage-queue";
+import cron from "node-cron"
+import fs from 'fs';
+import path from 'path';
+
+const configPath = path.resolve(__dirname, 'aqw.config.json');
+const rawConfig = fs.readFileSync(configPath, 'utf8');
+const config = JSON.parse(rawConfig);
+
+const queueServiceClient = QueueServiceClient.fromConnectionString(config.azureStorageConnectionString);
+const retries = config.retries ?? 3;
+
+class AzureQueueWrapper {
+    constructor() {
+        this.queueTasks = [];
+    }
+
+    addQueueTask(queueName, cronExpression, callback, maxRetries = retries, deadLetterQueueName = `${queueName}-poison`) {
+        this.queueTasks.push({ queueName, cronExpression, callback, maxRetries, deadLetterQueueName });
+    }
+
+    startQueueTasks() {
+        this.queueTasks.forEach(({ queueName, cronExpression, callback, maxRetries, deadLetterQueueName= `${queueName}-poison` }) => {
+            cron.schedule(cronExpression, async () => {
+                const queueClient = queueServiceClient.getQueueClient(queueName);
+                const messages = await queueClient.receiveMessages();
+                for (const message of messages.receivedMessageItems) {
+                    try {
+                        await callback(message);
+                        await queueClient.deleteMessage(message.messageId, message.popReceipt);
+                    } catch (error) {
+                        console.error('Error processing message:', error);
+                        if (message.dequeueCount > maxRetries) {
+                                const deadLetterQueueClient = queueServiceClient.getQueueClient(deadLetterQueueName);
+                                await deadLetterQueueClient.sendMessage(message.messageText);
+                                await queueClient.deleteMessage(message.messageId, message.popReceipt);
+                        }
+                    }
+                }
+            });
+        });
+    }
+}
+
+function ProcessAzureQueueMessage(options) {
+    return function (target, key) {
+        const { queue, timeInterval = [5, 'seconds'], maxRetries, deadLetterQueue } = options;
+        if (!queue) {
+            throw new Error(`Queue name is required for @ProcessAzureQueueMessage decorator`);
+        }
+        const [value, unit] = timeInterval;
+        const callback = target[key];
+        if (typeof callback === 'function') {
+            const cronExpression = convertTimeIntervalToCron(value, unit);
+            const azureQueue = new AzureQueueWrapper();
+            azureQueue.addQueueTask(queue, cronExpression, callback, maxRetries, deadLetterQueue);
+            azureQueue.startQueueTasks();
+        } else {
+            throw new Error(`@ProcessAzureQueueMessage decorator can only be applied to functions`);
+        }
+    };
+}
+
+
+function AddMessageToQueue() {
+    return function (target, key, descriptor) {
+        const originalMethod = descriptor.value;
+
+        descriptor.value = async function (queueName, message, ...args) {
+            if (!queueName) {
+                throw new Error('Queue name is required for AddMessageToQueue decorator');
+            }
+
+            try {
+                const response = await originalMethod.apply(this, [queueName, message, ...args]);
+
+                const queueClient = queueServiceClient.getQueueClient(queueName);
+                await queueClient.sendMessage(JSON.stringify(message));
+
+                return { status: 'success', response };
+            } catch (error) {
+                return { status: 'fail', error: error.message };
+            }
+        };
+
+        return descriptor;
+    };
+}
+
+function convertTimeIntervalToCron(value, unit) {
+    let cronExpression = '';
+    switch (unit.toLowerCase()) {
+        case 'seconds':
+            cronExpression = `*/${value} * * * * *`;
+            break;
+        case 'minutes':
+            cronExpression = `*/${value} * * * *`;
+            break;
+        case 'hours':
+            cronExpression = `0 */${value} * * *`;
+            break;
+        case 'days':
+            cronExpression = `0 0 */${value} * *`;
+            break;
+        case 'weeks':
+            cronExpression = `0 0 0 */${value * 7} *`;
+            break;
+        default:
+            throw new Error(`Unsupported time interval unit: ${unit}`);
+    }
+    return cronExpression;
+}
+
+module.exports = {
+    ProcessAzureQueueMessage,
+    AddMessageToQueue
+};
